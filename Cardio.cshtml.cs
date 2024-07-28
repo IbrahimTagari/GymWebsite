2e163c6a0795fb82b744cf461f4612c24d2eb86b
IbrahimTagari <ibrahimtagari28@gmail.com>
2024-07-28 23:10:00 +0200

    Add files via upload

---
+++ Cardio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GymWorkoutRoutines.Pages.Workouts
+{
+    public class CardioModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Any logic you want to execute when the page is requested.
+        }
+    }
+}
---
+++ Strength.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GymWorkoutRoutines.Pages.Workouts
+{
+    public class StrengthModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Any logic you want to execute when the page is requested.
+        }
+    }
+}
