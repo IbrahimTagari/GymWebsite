5f316b8178b1c0923aea2995e990e03d9d0c27eb
IbrahimTagari <ibrahimtagari28@gmail.com>
2024-07-28 23:19:39 +0200

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GymWorkoutRoutines.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Any logic you want to execute when the page is requested.
+        }
+    }
+}
